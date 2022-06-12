904de7f08b69f5cb02fe0e21cc14e3d52e99fd6b
Seth David Gyimah <esec26@gmail.com>
2022-06-12 12:05:33 +0000

    phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+function writeCards (cards) { 
+    let Thankyou = []
+    for (let i = 0; i < cards.length; i++) {
+     Thankyou.push (`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+      debugger;
+    }
+    return Thankyou;
+  }
+
+  function countDown(number){
+    while (number > 0){
+      console.log(number)
+      --number   
+    }
+  console.log(number)
+  }
+
+
+    
+
+
+
+  
